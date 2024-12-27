8cf04d2c380107464edaeedab0a8a1923fd393ab
ouarzeddine aymen <aymenalpha11@gmail.com>
2024-12-27 11:45:24 +0100

    add /masroofy to routers

--- src/Home/Navbar.jsx
+++ src/Home/Navbar.jsx
@@ -9,17 +9,17 @@ export default function Navbar() {
                 <a className="site-title">Masroofy</a>
                 <ul>
                     <li>
-                        <Link to="/" style={{ display: "block", padding: "10px 20px" }}>
+                        <Link to="/masroofy" style={{ display: "block", padding: "10px 20px" }}>
                             Home
                         </Link>
                     </li>
                     <li>
-                        <Link to="/chart" style={{ display: "block", padding: "10px 20px" }}>
+                        <Link to="/masroofy/chart" style={{ display: "block", padding: "10px 20px" }}>
                             Chart
                         </Link>
                     </li>
                     <li>
-                        <Link to="/about" style={{ display: "block", padding: "10px 20px" }}>
+                        <Link to="/masroofy/about" style={{ display: "block", padding: "10px 20px" }}>
                             About
                         </Link>
                     </li>
